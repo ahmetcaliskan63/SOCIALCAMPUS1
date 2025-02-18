4f1e2079ebec770bfdf6994e5c3163cc4c7b47a6
ahmetcaliskan63 <ahmetabclife@gmail.com>
2025-02-18 03:29:54 +0300

    Fix start script in package.json

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,20 +3,26 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: 'yamabiko.proxy.rlwy.net',
     port: 24760,
     user: 'root',
     password: 'XNpcNGoviOKfDNkHdBxpECMpFyMAmOnC',
-    database: 'railway'
+    database: 'railway',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 60000 // 60 saniye
 });
 
-connection.connect((err) => {   
+// Test bağlantısı
+connection.getConnection((err, conn) => {
     if (err) {
         console.error('Veritabanı bağlantı hatası:', err);
         return;
     }
     console.log('MySQL veritabanına bağlandı');
+    conn.release();
 });
 
-module.exports = connection; 
+module.exports = connection.promise(); 
